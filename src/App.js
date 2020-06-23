363414b3288456b49f10f2552fa7b5ac89564054
Allan Melo <allmelo@users.noreply.github.com>
2020-06-23 16:50:00 -0300

    List connection working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+
+  const [ repolist, setRepolist ] = useState([]);
+  
+  useEffect( () => {
+    api.get('/repositories')
+    .then( response => {
+      setRepolist(response.data)})
+    }, []);
+
   async function handleAddRepository() {
     // TODO
   }
@@ -14,13 +23,16 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
+        {repolist.map( repo => {
+          return (
+            <li key={repo.id}> {repo.title}
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
         </li>
+          )
+        })
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
