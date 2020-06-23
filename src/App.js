4cdb18fb26e5469dee286b13112f216ab02377d7
Allan Melo <allmelo@users.noreply.github.com>
2020-06-23 18:30:53 -0300

    Add & Remove repos from API

--- src/App.js
+++ src/App.js
@@ -4,37 +4,51 @@ import "./styles.css";
 
 function App() {
 
+  // Actual state of the list
   const [ repolist, setRepolist ] = useState([]);
-  
+
+  // Fetch and save repositories list from API 
   useEffect( () => {
     api.get('/repositories')
-    .then( response => {
-      setRepolist(response.data)})
-    }, []);
+    .then( response => {  setRepolist(response.data)  } )
+  }, []);  
 
+  // Add new repository to API list
   async function handleAddRepository() {
-    // TODO
-  }
+    
+    const response = await api.post('/repositories', 
+            { 
+              "title": "notch",
+              "url": "https://github.com/allmelo", 
+              "techs": ["javascript", "typescript", "node", "react", "reactnative"]
+            });
+    
+    setRepolist([...repolist, response.data]);
 
+  };
+
+  // Remove old repository from API list
   async function handleRemoveRepository(id) {
-    // TODO
+    
+    const response = await api.delete("/repositories/" + id);
+    setRepolist(repolist.filter( repo => repo.id !== id ));
+    console.log(response);
+
   }
 
+  // Render list
   return (
     <div>
       <ul data-testid="repository-list">
-        {repolist.map( repo => {
-          return (
-            <li key={repo.id}> {repo.title}
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-          )
-        })
+        { repolist.map( repo => { 
+            return (
+              <li id={repo.id} key={repo.id}><a href={repo.url}>{repo.title}</a>
+                <button onClick={() => handleRemoveRepository(repo.id)}> Remover </button>
+              </li>
+            )
+          })
         }
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
